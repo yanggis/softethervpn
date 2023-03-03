a4a94dc00020aa306b2011a43189282d6c542383
X.K. YANG <xiankungis@gmail.com>
2023-03-03 14:11:25 +0800

    Update start.sh

--- softether-vpn-client/start.sh
+++ softether-vpn-client/start.sh
@@ -6,8 +6,13 @@ IP_VPN_PRIVATE=192.168.30.1
 
 echo "Connect to VPN"
 echo -e "2\n\nAccountConnect $ACCOUNT\n" | /usr/local/vpnclient/vpncmd # Connect Account                                                        
+sleep 5
+
+echo "get new ip address"
+dhclient vpn_$VPN_INTERFACE
+sleep 10
 
 echo "remove redirect traffic"
 ip route del default via $IP_VPN_PRIVATE
 
-dhclient vpn_$VPN_INTERFACE
+
